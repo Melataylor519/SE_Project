6a9165ce4911d4fbde9df18b27d094358cb035f6
leah <dltjddms1028@gmail.com>
2025-01-29 13:48:18 -0500

    Update Test2.java

--- gradle/src/Test2.java
+++ gradle/src/Test2.java
@@ -3,11 +3,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		
-		for (int i = 0; i < 10; i++) { 
-			System.out.println("hello"); 
-			}
-		
-		int j=7;
+		System.out.println("hello");
 
 	}
 

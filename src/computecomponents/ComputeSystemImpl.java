4fbf1f1246930d73ba6f841d84e798267b2191bb
Melataylor519 <melataylor519@gmail.com>
2025-02-18 16:43:36 -0500

    Create ComputeSystemImpl
    Created empty ComputeSystem Implementation with it's method returning null

---
+++ src/computecomponents/ComputeSystemImpl.java
@@ -0,0 +1,10 @@
+package computecomponents;
+
+public class ComputeSystemImpl implements ComputeSystem{
+
+	@Override
+	public ComputeResponse compute(ComputeRequest computeRequest) {
+		return null;
+	}
+
+}

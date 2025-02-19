bf0427af10857e7762bb9f6070b5ac3f80c4d939
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-02-19 15:02:28 -0500

    Update DataProcessingImp.java

--- src/project/annotations/DataProcessingImp.java
+++ src/project/annotations/DataProcessingImp.java
@@ -1,23 +1,15 @@
 package project.annotations;
 
-/**
- * Implementation of DataProcessingAPI interface.
- */
 public class DataProcessingImp implements DataProcessingAPI {
 	private final DataProcessingAPI dataProcessAPI;
 	// Field to store API instance 
-	
-	/**
-	 * Constructor for DataProcessingImp.
-	 * @param dataProcessAPI The API instance to be used
-	 */
 	public DataProcessingImp(DataProcessingAPI dataProcessAPI) {
 		this.dataProcessAPI = dataProcessAPI;
 	}
 	
 	@Override
 	public ReadResult read(InputConfig input) {
-		return new ReadResultImp(ReadResult.Status.FAILURE, null);
+		return new ReadResultImp(ReadResult.Status.FAILURE, null, null);
 	}
 
 	@Override

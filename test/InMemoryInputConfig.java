3431194d002df1152bf66d005d0e977ef59a0f2d
Melataylor519 <melataylor519@gmail.com>
2025-02-19 13:54:31 -0500

    Create InMemoryInputConfig.java
    Created in memory implementation for the input configuration

---
+++ test/InMemoryInputConfig.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+public class InMemoryInputConfig implements InputConfig {
+	
+	private List<Integer> input = new ArrayList<>();
+	
+	
+	public InMemoryInputConfig(List<Integer> input) {
+		this.inputs.addAll(input)
+	}
+	
+	public List<Integer> getInput() {
+		return input;
+	}
+}

7ad058d1865ed5aca8597b92db29ab0d9b72bfc4
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-02-19 13:54:42 -0500

    Update DataProcessingImp.java

--- src/project/annotations/DataProcessingImp.java
+++ src/project/annotations/DataProcessingImp.java
@@ -26,32 +26,4 @@ public class DataProcessingImp implements DataProcessingAPI {
 	}
 } 
 
-/**
- * Implementation of ReadResult Interface.
- */
-class ReadResultImp implements ReadResult {
-	private final Status status;
-	private final Iterable<Integer> results; 
-		
-	/**
-	 * Constructor for ReadResultImp.
-	 * @param status the status of the read operation
-	 * @param results the results from the read operation
-	 */
-	public ReadResultImp(Status status, Iterable<Integer> results) {
-		this.status = status;
-		this.results = results;
-	}
-	
-	@Override	
-	public Status getStatus() {
-		return status;
-	}	
-	
-	@Override
-	public Iterable<Integer> getResults() {
-		return results;
-	}
-}
-
 

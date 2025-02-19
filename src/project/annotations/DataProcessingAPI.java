a27e69d8543386b7c4a52c77c64b6d820edf5ece
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-02-19 12:21:54 -0500

    Add files via upload

---
+++ src/project/annotations/DataProcessingAPI.java
@@ -0,0 +1,10 @@
+package project.annotations;
+
+@ProcessAPI
+public interface DataProcessingAPI {
+	    ReadResult read(InputConfig input);
+	    WriteResult appendSingleResult(OutputConfig output, String result, char delimiter);
+}
+
+	
+

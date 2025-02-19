3db64f5543347a3ef6a88b044ed1544277cf912a
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-02-19 12:55:10 -0500

    Try 2

---
+++ src/project/annotations/ReadResult.java
@@ -0,0 +1,13 @@
+package project.annotations;
+
+public interface ReadResult {
+	public static enum Status {
+	    SUCCESS,
+	    FAILURE
+	}
+
+	public Iterable<Integer> getResults();
+	    
+	public Status getStatus();
+
+}

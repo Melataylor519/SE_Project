aa8fe251f814d3c4c2a98e1331ab8361a4cc6aff
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-02-19 14:25:20 -0500

    Update TestComputeSystem.java

--- test/TestComputeSystem.java
+++ test/TestComputeSystem.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import computecomponents.ComputeResponse;
 import computecomponents.ComputeSystem;
 
 import static org.mockito.Mockito.when;
@@ -9,9 +10,15 @@ import static org.mockito.Mockito.any;
 	public class TestComputeSystem {
 		@Test
 		public void testComputeSystem() throws Exception {
-			ComputeSystem mockComputeSystem = new ComputeSystem();
-			ComputeRequest mockRequest = new ComputeRequest();
-			ComputeResponse mockResponse = new ComputeResponse();
-			when(mockComputeSystem.compute(mockRequest)).thenReturn(mockResponse);
-		}
+			
+			//initialize ComputeSystem
+			ComputeSystem system = new ComputeSystem();
+			
+			//mock dependencies
+			ComputeRequest mockRequest = Mockito.mock(ComputeRequest.class);
+			ComputeResponse response = system.compute(mockRequest);
+			
+			//return SUCCESS
+			Assertions.asserEquals(response.getStatus(),ComputeResponse.ComputeResponseStatus.SUCCESS);
+			
 	}

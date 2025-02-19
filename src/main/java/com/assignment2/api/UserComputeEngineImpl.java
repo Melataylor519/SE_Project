569a60f7792fc0341b3b24f7c7466cedb3463ef8
leah <dltjddms1028@gmail.com>
2025-02-19 13:04:19 -0500

    make smoke test file

--- src/main/java/com/assignment2/api/UserComputeEngineImpl.java
+++ src/main/java/com/assignment2/api/UserComputeEngineImpl.java
@@ -1,5 +1,19 @@
+// UserComputeEngineImpl.java
+
 package main.java.com.assignment2.api;
 
-public class UserComputeEngineImpl {
+import main.java.project.annotations.NetworkAPI;
 
+/**
+ * Empty implementation of UserComputeEngineAPI.
+ * This class serves as a placeholder for future functionality.
+ */
+
+@NetworkAPI
+public class UserComputeEngineImpl implements UserComputeEngineAPI {
+
+    @Override
+    public void processData(String inputSource, String outputSource, String[] delimiters) {
+        // TODO: Implement data processing logic
+    }
 }

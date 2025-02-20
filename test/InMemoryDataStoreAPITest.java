3f38d95b50bf9d34e36bdfd8297b1b139bcd0155
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-02-20 08:59:33 -0500

    Fix space in InMemoryDataStoreAPITest.java (#64)

--- test/InMemoryDataStoreAPITest.java
+++ test/InMemoryDataStoreAPITest.java
@@ -7,25 +7,25 @@ public class InMemoryDataStoreAPITest implements DataProcessingAPI {
 	@Override
 	public ReadResult read(InputConfig input) {
 		if (!(input instanceof InMemoryInputConfig)) {
-            throw new IllegalArgumentException("Unsupported InputConfig type");
-        }
+            		throw new IllegalArgumentException("Unsupported InputConfig type");
+        	}
 
-        InMemoryInputConfig inMemoryInput = (InMemoryInputConfig) input;
-        List<Integer> inputData = inMemoryInput.getInput();
+        	InMemoryInputConfig inMemoryInput = (InMemoryInputConfig) input;
+        	List<Integer> inputData = inMemoryInput.getInput();
 
-        return new ReadResultImp(ReadResult.Status.SUCCESS, inputData);
+        	return new ReadResultImp(ReadResult.Status.SUCCESS, inputData);
 	}
 
 	@Override
 	public WriteResult appendSingleResult(OutputConfig output, String result, char delimiter) {
-		 if (!(output instanceof InMemoryOutputConfig)) {
-			 throw new IllegalArgumentException("Unsupported OutputConfig type");
-	     }
+		if (!(output instanceof InMemoryOutputConfig)) {
+			throw new IllegalArgumentException("Unsupported OutputConfig type");
+	     	}
 		 
-		 InMemoryOutputConfig inMemoryOutput = (InMemoryOutputConfig) output;
-	     inMemoryOutput.getOutput().add(result);
+		InMemoryOutputConfig inMemoryOutput = (InMemoryOutputConfig) output;
+		inMemoryOutput.getOutput().add(result);
 
-	     return new WriteResultImp(WriteResult.WriteResultStatus.SUCCESS);
+	     	return new WriteResultImp(WriteResult.WriteResultStatus.SUCCESS);
 	}
 
 }

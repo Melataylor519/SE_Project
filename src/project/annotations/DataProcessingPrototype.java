1481eb88c606f443874978876e829461ff1bb09b
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-02-27 02:36:57 -0500

    Fix code in DataProcessingPrototype.java

--- src/project/annotations/DataProcessingPrototype.java
+++ src/project/annotations/DataProcessingPrototype.java
@@ -14,7 +14,10 @@ public class DataProcessingPrototype implements DataProcessingAPI {
 	
 	public void prototype(DataProcessingAPI apiCall) {
 		InputConfig inputConfig = new InputConfig(){
-			
+			@Override
+    			public String getFilePath() {
+        			return "";  // replace with the actual file path
+    			}
 		}; 
 		OutputConfig outputConfig = null;
 

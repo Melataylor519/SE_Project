2be5eb744ad97e57623875239cebff4925292bf3
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-02-27 17:41:30 -0500

    Space in OutputConfig.java

--- src/project/annotations/OutputConfig.java
+++ src/project/annotations/OutputConfig.java
@@ -3,5 +3,5 @@ package project.annotations;
 public interface OutputConfig {
   String getFilePath();
   
-	String formatOutput(String result);  // return output result as String
+  String formatOutput(String result);  // return output result as String
 }

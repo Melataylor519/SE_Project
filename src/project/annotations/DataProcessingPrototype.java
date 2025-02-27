475b8fd30dd756be893ed161086c90230f60d4b4
leah-1ee <dltjddms1028@gmail.com>
2025-02-27 11:45:43 -0500

    Update DataProcessingPrototype.java

--- src/project/annotations/DataProcessingPrototype.java
+++ src/project/annotations/DataProcessingPrototype.java
@@ -85,29 +85,5 @@ public class DataProcessingPrototype implements DataProcessingAPI {
 
 
 
-	
-	public void prototype(DataProcessingAPI apiCall) {
-		InputConfig inputConfig = new InputConfig(){
-			@Override
-    			public String getFilePath() {
-        			return "";  // replace with the actual file path
-    			}
-		}; 
-		OutputConfig outputConfig = null;
 
-		ReadResult dataStoreReadResult = apiCall.read(inputConfig);
 
-		if (dataStoreReadResult.getStatus() == ReadResult.Status.SUCCESS) {
-			Iterable<Integer> loadedData = dataStoreReadResult.getResults();
-
-			for (int i : loadedData) {
-				String result = "" + i;
-				WriteResult writeResult = apiCall.appendSingleResult(outputConfig, result, ',');
-
-				if (writeResult.getStatus() != WriteResult.WriteResultStatus.SUCCESS) {
-					System.out.println("Fail. Please try again.");
-				}
-			}
-		}
-	}
-}

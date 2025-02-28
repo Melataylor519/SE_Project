e230960f79a91b11ef987bed286a7fd7731d3005
Melataylor519 <melataylor519@gmail.com>
2025-02-28 13:17:31 -0500

    made a couple changes to TestComputeSystem
    added mock dependencies for implemented compute system. updated assertion to account for ComputeSystemImpl method that checks for null or empty input

--- test/TestComputeSystem.java
+++ test/TestComputeSystem.java
@@ -1,25 +1,53 @@
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import computecomponents.ComputeRequest;
 import computecomponents.ComputeResponse;
-import computecomponents.ComputeSystem;
+import computecomponents.ComputeSystemImpl;
+import computecomponents.ComputeSystemApi;
+import main.java.com.assignment2.api.UserComputeEngineAPI;
+import project.annotations.DataProcessingAPI;
+import project.annotations.InputConfig;
+import project.annotations.OutputConfig;
+import project.annotations.ReadResult.Status;
+import project.annotations.ReadResultImp;
 
 
 	public class TestComputeSystem {
 		@Test
 		public void testComputeSystem() throws Exception {
 			
-			//initialize ComputeSystem
-			ComputeSystem system = Mockito.mock(ComputeSystem.class);
-			
 			//mock dependencies
-			ComputeRequest mockRequest = Mockito.mock(ComputeRequest.class);
+			DataProcessingAPI mockDP = Mockito.mock(DataProcessingAPI.class);
+			UserComputeEngineAPI mockEngine = Mockito.mock(UserComputeEngineAPI.class);
+			
+			Status status = Status.SUCCESS;
+			Iterable<Integer> results = new ArrayList<Integer>();
+			
+			
+			when(mockDP.read(any(InputConfig.class)))
+			   .thenReturn(new ReadResultImp(status, results));
+			
+			ComputeSystemImpl system = new ComputeSystemImpl(mockDP, mockEngine);
+			
+			//mock parameters
+			ComputeRequest mockRequest = Mockito.mock(ComputeRequest.class);	
+			when(mockRequest.getInputConfig()).thenReturn(mock(InputConfig.class));
+	        when(mockRequest.getOutputConfig()).thenReturn(mock(OutputConfig.class));
 			ComputeResponse response = system.compute(mockRequest);
 			
-			//return SUCCESS
-			Assertions.assertEquals(response.getStatus(),ComputeResponse.ComputeResponseStatus.SUCCESS);
+			
+			
+			//return INVALID_REQUEST because test input is null or empty
+			Assertions.assertEquals(response.getStatus(),ComputeResponse.ComputeResponseStatus.INVALID_REQUEST);
 			
 	}
 }

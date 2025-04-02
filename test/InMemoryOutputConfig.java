87eaf9204f3341d9a9992d6dd0bb63b588a64f61
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-04-02 13:18:06 -0400

    Fix imports in InMemoryOutputConfig.java

--- test/InMemoryOutputConfig.java
+++ test/InMemoryOutputConfig.java
@@ -1,25 +1,27 @@
+package test;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import project.annotations.OutputConfig;
+import src.datastorecomponents.OutputConfig;
 
 public class InMemoryOutputConfig  implements OutputConfig {
-		private List<String> output = new ArrayList<>();
+	private List<String> output = new ArrayList<>();
 		
 		
-		public List<String> getOutput() {
-			return output;
-		}
+	public List<String> getOutput() {
+		return output;
+	}
 
-		@Override
-		public String getFilePath() {
-			// TODO Auto-generated method stub
-			return null;
-		}
+	@Override
+	public String getFilePath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-		@Override
-		public String formatOutput(String result) {
-			// TODO Auto-generated method stub
-			return null;
-		}
+	@Override
+	public String formatOutput(String result) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }

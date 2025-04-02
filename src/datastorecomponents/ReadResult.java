20bf74159f4e05ccbcf254614063f192a9599e7e
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-04-02 13:07:19 -0400

    Create ReadResult.java

---
+++ src/datastorecomponents/ReadResult.java
@@ -0,0 +1,13 @@
+package datastorecomponents;
+
+public interface ReadResult {
+	public static enum Status {
+	    SUCCESS,
+	    FAILURE;
+	}
+
+	public Iterable<Integer> getResults();
+	    
+	public Status getStatus();
+
+}

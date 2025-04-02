c37fdcb828c89123f445e076a457385cd43369d1
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-04-02 13:25:59 -0400

    Fix typo in TestUser.java

--- test/TestUser.java
+++ test/TestUser.java
@@ -1,8 +1,12 @@
-import java.io.File;
+package test;
 
+import java.io.File;
+import src.usercomputecomponents.UserComputeEngineAPI;
 
 public class TestUser {
 	
+	// TODO 3: change the type of this variable to the name you're using for your
+	// @NetworkAPI interface; also update the parameter passed to the constructor
 	private final UserComputeEngineAPI coordinator;
 
 	public TestUser(UserComputeEngineAPI coordinator) {
@@ -12,12 +16,13 @@ public class TestUser {
 	public void run(String outputPath) {
 		char delimiter = ';';
 		String inputPath = "test" + File.separatorChar + "testInputFile.test";
+		
+		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
+		// run the compute job specified by inputPath, outputPath, and delimiter
+		// processData() receives delimiter as a String array, so convert it to an array
+	    	String[] delimiters = {String.valueOf(delimiter)};
 
-	    // processData() receives delimiter as a String array, so convert it to an array
-	    String[] delimiters = {String.valueOf(delimiter)};
-
-	    // data process
-	    coordinator.processData(inputPath, outputPath, delimiters);
+	    	// data process
+	    	coordinator.processData(inputPath, outputPath, delimiters);
 	}
-
 }

02a5918ff84f096837a262674dae70be6ae342ea
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-04-08 15:09:02 -0400

    Delete some unused methods in InputConfig.java

--- src/main/java/datastorecomponents/InputConfig.java
+++ src/main/java/datastorecomponents/InputConfig.java
@@ -1,15 +1,5 @@
 package datastorecomponents;
 
-/**
- * Interface for configuring input data sources.
- * Implementations should provide the path to the input file
- * and handle any input-specific configuration.
- */
 public interface InputConfig {
-	/**
-	 * Gets the file path for the input data.
-	 * 
-	 * @return The path to the input file
-	 */
 	String getFilePath();
 }

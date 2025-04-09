400e92671c3cbe239441a30de6f6c31d6159e5bc
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-04-09 00:31:36 -0400

    Delete src/projectannotations directory

--- src/projectannotations/NetworkAPIPrototype.java
+++
@@ -1,9 +0,0 @@
-package projectannotations;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target(ElementType.METHOD)
-public @interface NetworkAPIPrototype {
-	// Marker annotation, should be applied to a method within a prototype class
-}

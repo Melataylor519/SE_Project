6aeb811e84e588d6430ec29eb3094742a594ddea
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-04-09 00:41:49 -0400

    Delete src/main/projectannotations directory

--- src/main/projectannotations/ProcessAPI.java
+++
@@ -1,9 +0,0 @@
-package projectannotations;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target(ElementType.TYPE)
-public @interface ProcessAPI {
-	// Marker annotation, should be applied to an interface type
-}

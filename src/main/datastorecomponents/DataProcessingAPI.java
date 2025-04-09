6b0b4361a8521d46bb1cdfcdab454fa58f25e82d
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-04-09 00:41:42 -0400

    Delete src/main/datastorecomponents directory

--- src/main/datastorecomponents/DataProcessingAPI.java
+++
@@ -1,9 +0,0 @@
-package datastorecomponents;
-
-import projectannotations.ProcessAPI;
-
-@ProcessAPI
-public interface DataProcessingAPI {
-    ReadResult read(InputConfig input);
-    WriteResult appendSingleResult(OutputConfig output, String result, char delimiter);
-}

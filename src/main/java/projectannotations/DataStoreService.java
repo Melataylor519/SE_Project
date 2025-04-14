2e9db1025649a1e83f140fd49cebfb4f5f3f75de
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-04-14 14:22:57 -0400

    Fix Imports DataStoreService.java

--- src/main/java/projectannotations/DataStoreService.java
+++ src/main/java/projectannotations/DataStoreService.java
@@ -1,7 +1,7 @@
 package main.java.projectannotations;
 
-import main.protobuf.datastore.DataProcessingGrpc;
-import main.protobuf.datastore.DatastoreProto;
+import datastore.DataProcessingGrpc;
+import datastore.DatastoreProto;
 import io.grpc.stub.StreamObserver;
 import main.java.datastorecomponents.DataProcessingAPI;
 import main.java.datastorecomponents.DataProcessingImp;

47013c873c4c2750f0e0f97703cea2e365a53419
lanphan03 <161795474+lanphan03@users.noreply.github.com>
2025-05-13 21:11:11 -0400

    Fix import TestUser.java

--- src/test/java/TestUser.java
+++ src/test/java/TestUser.java
@@ -1,28 +1,20 @@
-import java.io.File;
+import projectannotations.DataStoreClient;
 import usercomputecomponents.UserComputeEngineAPI;
-import datastorecomponents.DataStoreClient;
 
 public class TestUser {
-	
-	// TODO 3: change the type of this variable to the name you're using for your
-	// @NetworkAPI interface; also update the parameter passed to the constructor
 	private final UserComputeEngineAPI coordinator;
+	private final String inputPath;
+	private final char delimiter;
 
-	public TestUser(UserComputeEngineAPI coordinator) {
+	public TestUser(UserComputeEngineAPI coordinator, String inputPath, char delimiter) {
 		this.coordinator = coordinator;
+		this.inputPath = inputPath;
+		this.delimiter = delimiter;
 	}
-
+	
 	public void run(String outputPath) {
-		char delimiter = ';';
-		String inputPath = "src/test/java/TestInputFile.test";
 		DataStoreClient client = DataStoreClient.connect("localhost:50052");
-		
-		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
-		// run the compute job specified by inputPath, outputPath, and delimiter
-		// processData() receives delimiter as a String array, so convert it to an array
 		String[] delimiters = {String.valueOf(delimiter)};
-
-		// data process
 		coordinator.processData(client, inputPath, outputPath, delimiters);
 	}
 }
